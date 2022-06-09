c01b2b4443816cf9c98c5e78da52dcbd2fbba43f
Laura Behan <laurarbehan@gmail.com>
2022-06-09 16:58:12 -0400

    finished part 3

--- crud.py
+++ crud.py
@@ -12,6 +12,24 @@ def create_user(email, password):
     return user
 
 
+def get_users():
+    """return all users"""
+    
+    return User.query.all()
+
+
+def get_user_by_id(user_id):
+    """return user by id"""
+
+    return User.query.get(user_id)
+
+
+def get_user_by_email(email):
+    """Return a user by email."""
+
+    return User.query.filter(User.email == email).first()
+
+
 def create_movie(title, overview, release_date, poster_path):
     """Create and return a new movie."""
 
@@ -26,6 +44,13 @@ def get_all_movies():
     return Movie.query.all()
 
 
+def get_movie_by_id(movie_id):
+    """Return a movie by movie_id"""
+
+    return Movie.query.get(movie_id)
+
+
+
 def create_rating(user, movie, score):
     """Create a rating."""
 
@@ -34,10 +59,7 @@ def create_rating(user, movie, score):
     return rating
 
 
-def get_movie_by_id(movie_id):
-    """Return a movie by movie_id"""
 
-    return Movie.query.get(movie_id)
 
 if __name__ == '__main__':
     from server import app
--- server.py
+++ server.py
@@ -25,12 +25,54 @@ def get_all_movies():
 
     return render_template("all_movies.html", movies=movies)
 
+
 @app.route('/movies/<movie_id>')
 def show_movie(movie_id):
     """Show details on a particular movie."""
 
-    movie = crud.get_movie_by_id()
+    movie = crud.get_movie_by_id(movie_id)
 
+    return render_template("movie_details.html", movie=movie)
+
+@app.route('/users')
+def get_all_users():
+    """View all users"""
+
+    users = crud.get_users()
+
+    return render_template("all_users.html", users=users)
+
+
+@app.route('/users/<user_id>')
+def show_user(user_id):
+    """Show one user info"""
+
+    user = crud.get_user_by_id(user_id)
+
+    return render_template("user_details.html", user=user)
+    
+
+@app.route('/users', methods=["POST"])
+def register_user():
+    """create a new user"""
+
+    email1 = request.form.get("email") #form email
+    password = request.form.get("password")
+    # email1 - data
+    # email2 - form
+    user = crud.get_users()
+    
+    # for item in crud.get_users: #<email="asdasdsa@" passowrd='asdasd>
+    if user.email == email1:
+        flash(f'Cannot create an account with that e-mail') 
+    else:
+        user = crud.create_user(email1, password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Account created please log in')
+        return redirect('/')
+    
+    return render_template("user_details.html", item=item, user=user)
 
 
 
